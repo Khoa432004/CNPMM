d918816b7ed028a1e8477fcc4fafaa7bd78d1f05
Khoa <157439923+Khoa432004@users.noreply.github.com>
2025-11-05 10:19:06 +0700

    init project

---
+++ my-profile/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="profile-container">
+      <div className="profile-card">
+        <h1>Thông tin cá nhân</h1>
+        <div className="info-item">
+          <p><strong>Họ và tên:</strong> Đỗ Nguyễn Đăng KhoaKhoa</p>
+        </div>
+        <div className="info-item">
+          <p><strong>MSSV:</strong> 22110167</p>
+        </div>
+        <div className="info-item">
+          <p><strong>Lớp:</strong> 22110CL3B</p>
+        </div>
+        <div className="info-item">
+          <p><strong>Email:</strong> donguyendangkhoa0403@gmail.com</p>
+        </div>
+        <div className="info-item">
+          <p><strong>SĐT:</strong> 0773914830</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
